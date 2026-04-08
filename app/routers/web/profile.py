c69e3025eb1b834491911090e7f35d1e6ab6fba5
JonFernandesAizcorbe <jonpo1992@hotmail.es>
2026-04-08 15:29:04 +0200

    feat: enhance user profile page with activity tracking and last watched anime

--- app/routers/web/profile.py
+++ app/routers/web/profile.py
@@ -1,4 +1,6 @@
 
+from datetime import datetime
+
 from fastapi import APIRouter, Depends, Request, Response
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -7,6 +9,7 @@ from sqlalchemy.orm import Session
 
 from app.database import get_db
 from app.models.anime import AnimeORM
+from app.models.anime_list import AnimeListORM
 from app.models.user import UserORM
 from app.auth import user_require
 
@@ -16,9 +19,21 @@ templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/", response_class=HTMLResponse)
 def user_required(request: Request, db: Session = Depends(get_db), user: UserORM = Depends(user_require)):
+    date = datetime.now()
     anime = db.execute(select(AnimeORM)).scalars().first()
+    last_animes = db.execute(select(AnimeListORM).where(AnimeListORM.user_id == user.id, AnimeListORM.status != "Eliminado").order_by(AnimeListORM.date_like.asc()).limit(5)).scalars().all()
+    activity = db.execute(select(AnimeListORM).where(AnimeListORM.user_id == user.id, AnimeListORM.date_like < date).order_by(AnimeListORM.date_like.desc()).limit(4)).scalars().all()
+
+    hours = []
+
+    for a in activity:
+        time = (date - a.date_like).total_seconds() / 3600 # convert to hours
+        hours.append(time)
+
+
     return templates.TemplateResponse(
         "profile/profile.html",
-        {"request": request, "user": user, "anime": anime}
+        {"request": request, "user": user, "anime": anime, "activity": activity, "hours": hours, "last_animes": last_animes}
     )
+
         
